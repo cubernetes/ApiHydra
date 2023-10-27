83ff583476dca24f868cb1ba70b242d0173cdcb2
tosu <timo42@proton.me>
2023-10-27 03:44:07 +0200

    FtApiHydra can now create and delete Apps + consistent logging

--- ApiHydra.py
+++ ApiHydra.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 import json
 import time
 import copy
 import requests
 import threading
+from datetime import datetime
 from typing import Any, TextIO
 from collections import defaultdict
 from abc import ABC, abstractmethod
@@ -50,8 +52,8 @@ class ApiHydra(ABC):
         self.thread_counter = 0
         self.deserialize(self.apps_file)
         self.number_of_caller_requests = 0
-        self.number_of_200_requests = 0
-        self.number_of_non_200_requests = 0
+        self.number_of_ok_requests = 0
+        self.number_of_non_ok_requests = 0
         self.response_bytes = 0
 
     def __del__(self):
@@ -59,9 +61,9 @@ class ApiHydra(ABC):
         stats_log_level = INFO if self.stats else DEBUG
         self.log(f'Statistics:', stats_log_level)
         self.log(f'- {self.number_of_caller_requests} caller requests', stats_log_level)
-        self.log(f'- {self.number_of_200_requests + self.number_of_non_200_requests} actual requests', stats_log_level)
-        self.log(f'-- {self.number_of_200_requests} successful (200) requests', stats_log_level)
-        self.log(f'-- {self.number_of_non_200_requests} unsuccessful requests', stats_log_level)
+        self.log(f'- {self.number_of_ok_requests + self.number_of_non_ok_requests} actual requests', stats_log_level)
+        self.log(f'-- {self.number_of_ok_requests} successful (200, ...) requests', stats_log_level)
+        self.log(f'-- {self.number_of_non_ok_requests} unsuccessful requests', stats_log_level)
         self.log(f'- {self.response_bytes} response bytes', stats_log_level)
 
     def deserialize(self, apps_file: str=DEFAULT_APPS_FILE):
@@ -142,7 +144,7 @@ class ApiHydra(ABC):
         """
         if not args and 'url' not in kwargs:
             self.log(f'{threading.current_thread().name}: Call to {self.__class__.__name__}._get requires at least url argument (either as 1st arg or as kwarg).', ERROR)
-            return;
+            return
         elif 'url' in kwargs:
             url = kwargs['url']
             if url.startswith('/'):
@@ -159,21 +161,22 @@ class ApiHydra(ABC):
         while True:
             if retries > self.max_retries:
                 self.log(f'{threading.current_thread().name}: Data loss: get request to "{url}" and "{args=}", "{kwargs=}" exceeded max retries ({self.max_retries}).', ERROR)
-                return;
+                return
             app = self.get_next_app()
             kwargs = self.make_request_kwargs_from_app(app, **kwargs)
             resp = requests.get(*args, **kwargs)
-            if resp.status_code == 200:
-                self.number_of_200_requests += 1
+            if resp.status_code != 200:
+                self.number_of_non_ok_requests += 1
+                failed_req_log_level = DEBUG if resp.status_code == 429 else WARNING
+                self.log(f'{threading.current_thread().name}: Could not get "{url}" ({resp.status_code}) (retries: {retries}).', failed_req_log_level)
+                time.sleep(delay)
+                delay *= self.retry_delay_factor
+            else:
+                self.number_of_ok_requests += 1
                 self.log(f'{threading.current_thread().name}: Successful get: "{url}".', DEBUG)
                 self.response_bytes += len(resp.content)
                 self.responses.append((url, resp))
-                return;
-            else:
-                self.number_of_non_200_requests += 1
-                self.log(f'{threading.current_thread().name}: Could not get "{url}" ({resp.status_code}) (retries: {retries}).', DEBUG)
-                time.sleep(delay)
-                delay *= self.retry_delay_factor
+                return
             retries += 1
 
     def join(self):
@@ -256,6 +259,12 @@ class FtApiHydra(ApiHydra):
         )
         self.intra_login = intra_login
         self.intra_password = intra_password
+        self.session = None
+        self.is_updated = False
+
+    def ensure_session(self):
+        if not self.session:
+            self.session = self.create_intra_session(self.intra_login, self.intra_password)
 
     def make_request_kwargs_from_app(self, app: dict, **kwargs) -> dict[str, Any]:
         """Intra API implements bearer authentication.
@@ -283,21 +292,39 @@ class FtApiHydra(ApiHydra):
         session = requests.Session()
         self.log(f'Getting sign in page ({sign_in_page_url}).', DEBUG)
         r_initial = session.get(sign_in_page_url)
+        self.number_of_caller_requests += 1
+        if r_initial.status_code != 200:
+            self.number_of_non_ok_requests += 1
+            self.log(f'Could not get sign in page ({r_initial.status_code}) to create session.', ERROR)
+            return session
+        self.number_of_ok_requests += 1
         html_content = r_initial.text
         soup = BeautifulSoup(html_content, 'html.parser')
         element = soup.find(id='kc-form-login')
         authenticate_url = element.attrs.get('action', '') if isinstance(element, Tag) else ''
         if not authenticate_url:
-            self.log(f'Could not extract authentication url (empty).', ERROR)
+            self.log(f'Could not extract authentication url (empty) to create session.', ERROR)
             return session
         self.log(f'Posting to authentication url ({authenticate_url}).', DEBUG)
-        session.post(authenticate_url, data={'username': intra_login, 'password': intra_password})
+        post_creds_resp = session.post(authenticate_url, data={'username': intra_login, 'password': intra_password})
+        self.number_of_caller_requests += 1
+        if post_creds_resp.status_code != 200:
+            self.number_of_non_ok_requests += 1
+            self.log(f'Could not post credentials ({post_creds_resp.status_code}) to create session.', ERROR)
+            return session
+        self.number_of_ok_requests += 1
         self.log(f'Getting callback url ({callback_url}).', DEBUG)
-        session.get(callback_url)
-        self.log(f'Intra session creation successful. Returning session.', INFO)
+        callback_resp = session.get(callback_url)
+        self.number_of_caller_requests += 1
+        if callback_resp != 200:
+            self.number_of_non_ok_requests += 1
+            self.log(f'Could not get callback url ({callback_resp.status_code}) to create session.', ERROR)
+        else:
+            self.number_of_ok_requests += 1
+            self.log(f'Intra session creation successful.', INFO)
         return session
 
-    def get_token(self, uid: str, secret: str):
+    def get_token(self, app_id: str, uid: str, secret: str) -> str:
         """Requests an application access token using uid and secret.
         """
         try:
@@ -306,45 +333,125 @@ class FtApiHydra(ApiHydra):
                 'client_id': uid,
                 'client_secret': secret,
             })
+            self.number_of_caller_requests += 1
         except KeyboardInterrupt:
             print(end='\n', file=sys.stdout, flush=True)
             self.log(f'Received KeyboardInterrupt.', FATAL)
             raise SystemExit(130)
-        if resp.status_code == 200:
+        if resp.status_code != 200:
+            self.number_of_non_ok_requests += 1
+            self.log(f'Could not get access token ({resp.status_code}) for app "{app_id}".', ERROR)
+            return ''
+        else:
+            self.number_of_ok_requests += 1
             resp = resp.json()
             expires_in = resp['expires_in']
             token = resp['access_token']
             self.log(f'- {token=} {expires_in=}s\n', DEBUG)
             return token
-        return ''
 
-    def get_credentials(self, app_id: str) -> tuple[str, str]:
+    def update_app(self, app_id: str) -> None:
         """Scrape uid and secret from a given API application from the intra.
         """
+        self.ensure_session()
         self.log(f'Getting app credentials (uid, secret) for app "{app_id}".', DEBUG)
         resp = self.session.get(f'https://profile.intra.42.fr/oauth/applications/{app_id}')
+        self.number_of_caller_requests += 1
         if resp.status_code != 200:
-            self.log(f'Could not get page for app "{app_id}".', ERROR)
-            return '', ''
+            self.number_of_non_ok_requests += 1
+            self.log(f'Could not get page ({resp.status_code}) to update app "{app_id}".', ERROR)
+            return
+        self.number_of_ok_requests += 1
         html = resp.text
         soup = BeautifulSoup(html, 'html.parser')
+
         secret_div = soup.find('div', {'data-copy': f'[data-app-secret-{app_id}]'}, class_='copy')
         secret = secret_div.get('data-clipboard-text', '') if isinstance(secret_div, Tag) else ''
         uid_div = soup.find('div', {'data-copy': f'[data-app-uid-{app_id}]'}, class_='copy')
         uid = uid_div.get('data-clipboard-text', '') if isinstance(uid_div, Tag) else ''
-        if isinstance(uid, str) and isinstance(secret, str):
-            self.log(f'Successfully scraped credentials for app "{app_id}".', DEBUG)
-            return uid, secret
-        return '', ''
+
+        secrets = soup.find_all('div', class_='credential')
+        if len(secrets) > 2:
+            next_secret = soup.find_all('div', class_='credential')[2].text.strip()
+            if next_secret == '-':
+                next_secret = None
+        else:
+            next_secret = None
+
+        app_info = soup.find('div', class_='application-info')
+        app_name_span = app_info.find('span') if isinstance(app_info, Tag) else None
+        app_name = app_name_span.text if isinstance(app_name_span, Tag) else ''
+
+        app_details = list(soup.find_all('div', class_='row'))[2]
+
+        validity_text_divs = soup.find_all('div', class_='rotation-actions')
+        current_secret_text = validity_text_divs[0].text
+        next_secret_text = validity_text_divs[0].text
+        current_secret_expiry_date_match = re.search(r'\d+/\d+/\d+', current_secret_text)
+        next_secret_expiry_date_match = re.search(r'\d+/\d+/\d+', next_secret_text)
+        if current_secret_expiry_date_match:
+            current_secret_expiry_ts = int(datetime.strptime(current_secret_expiry_date_match.group(0), '%d/%m/%Y').timestamp())
+        else:
+            current_secret_expiry_ts = -1
+        if next_secret_expiry_date_match:
+            next_secret_expiry_ts = int(datetime.strptime(next_secret_expiry_date_match.group(0), '%d/%m/%Y').timestamp())
+        else:
+            next_secret_expiry_ts = -1
+
+        h4s = app_details.find_all('h4')
+        request_last_hour = int(h4s[0].next_sibling.next_sibling.find('span').text.strip())
+        max_requests_per_hour = int(h4s[0].next_sibling.next_sibling.find_all('span')[1].next_sibling.strip().lstrip('/').strip())
+        max_requests_per_second = int(soup.find_all('div', class_='application-desc')[1].find('code').text.strip())
+        active_tokens = int(h4s[1].next_sibling.next_sibling.text.strip())
+        active_users = int(h4s[2].next_sibling.next_sibling.text.strip())
+        total_requests = int(h4s[3].next_sibling.next_sibling.text.strip())
+        total_generated_tokens = int(h4s[4].next_sibling.next_sibling.text.strip())
+        total_unique_users = int(h4s[5].next_sibling.next_sibling.text.strip())
+
+        header = soup.find('div', class_='header')
+        if header:
+            redirect_code_block = header.find('code')
+            if isinstance(redirect_code_block, Tag):
+                redirect_url = redirect_code_block.text.strip()
+            else:
+                redirect_url = ''
+        else:
+            redirect_url = ''
+
+        scopes = []
+        for node in soup.find_all('label', class_='label-primary'):
+            scopes.append(node.text.strip())
+
+        self.apps[app_id]['uid'] = uid
+        self.apps[app_id]['secret'] = secret
+        self.apps[app_id]['next_secret'] = next_secret
+        self.apps[app_id]['app_name'] = app_name
+        self.apps[app_id]['requests_last_hour'] = request_last_hour
+        self.apps[app_id]['max_request_per_hour'] = max_requests_per_hour
+        self.apps[app_id]['max_requests_per_seconds'] = max_requests_per_second
+        self.apps[app_id]['active_tokens'] = active_tokens
+        self.apps[app_id]['active_users'] = active_users
+        self.apps[app_id]['total_requests'] = total_requests
+        self.apps[app_id]['total_generated_tokens'] = total_generated_tokens
+        self.apps[app_id]['total_unique_users'] = total_unique_users
+        self.apps[app_id]['current_secret_expiry_ts'] = current_secret_expiry_ts
+        self.apps[app_id]['next_secret_expiry_ts'] = next_secret_expiry_ts
+        self.apps[app_id]['redirect_url'] = redirect_url
+        self.apps[app_id]['scopes'] = scopes
+        self.log(f'Successfully scraped credentials for app "{app_id}".', DEBUG)
 
     def get_app_ids(self) -> list[str]:
         """Scrape user API applications from the intra.
         """
+        self.ensure_session()
         self.log(f'Getting app ids.', DEBUG)
         resp = self.session.get(f'https://profile.intra.42.fr/oauth/applications')
+        self.number_of_caller_requests += 1
         if resp.status_code != 200:
-            self.log(f'Could not get apps overview page.', ERROR)
+            self.number_of_non_ok_requests += 1
+            self.log(f'Could not get apps overview page ({resp.status_code}).', ERROR)
             return []
+        self.number_of_ok_requests += 1
         html = resp.text
         soup = BeautifulSoup(html, 'html.parser')
         apps_root = soup.find('div', class_='apps-root')
@@ -353,28 +460,88 @@ class FtApiHydra(ApiHydra):
         app_ids = [str(app_data['id']) for app_data in apps_data if 'id' in app_data]
         return app_ids
 
-    def refresh_tokens(self):
+    def refresh_tokens(self) -> None:
         """Only refresh the access tokens (fast)
         """
         for app_id, app_creds in self.apps.items():
             self.log(f'Refreshing token for app "{app_id}".', INFO)
             uid = app_creds['uid']
             secret = app_creds['secret']
-            token = self.get_token(uid, secret)
+            token = self.get_token(app_id, uid, secret)
             self.apps[app_id]['token'] = token
 
-    def update(self):
+    def delete_app(self, app_id: str) -> None:
+        """Delete API intra app by id.
+        """
+        self.ensure_session()
+        app_page = self.session.get(f'https://profile.intra.42.fr/oauth/applications/{app_id}')
+        self.number_of_caller_requests += 1
+        if app_page.status_code != 200:
+            self.number_of_non_ok_requests += 1
+            self.log(f'Could not get page ({app_page.status_code}) to delete app "{app_id}".', ERROR)
+            return
+        self.number_of_ok_requests += 1
+        soup = BeautifulSoup(app_page.text, 'html.parser')
+        csrf_token_meta = soup.find('meta', {'name': 'csrf-token'})
+        if isinstance(csrf_token_meta, Tag):
+            authenticty_token = csrf_token_meta.get('content', '')
+            resp = self.session.post(f'https://profile.intra.42.fr/oauth/applications/{app_id}',
+                  data={'_method': 'delete', 'authenticity_token': authenticty_token})
+            self.number_of_caller_requests += 1
+            if resp.status_code != 200:
+                self.number_of_non_ok_requests += 1
+                self.log(f'Could not delete app "{app_id}" ({resp.status_code}).', ERROR)
+            else:
+                self.number_of_ok_requests += 1
+                if app_id in self.apps:
+                    del self.apps[app_id]
+                self.log(f'Deleted app "{app_id}" successfully.', INFO)
+        else:
+            self.log(f'Could not delete app "{app_id}", could not find authenticity token.', ERROR)
+
+    def create_app(self):
+        """Create new API intra app with prefix "ApiHydra_" followed by index.
+        """
+        if not self.is_updated:
+            self.update()
+        apps_page = self.session.get(f'https://profile.intra.42.fr/oauth/applications')
+        self.number_of_caller_requests += 1
+        if apps_page.status_code != 200:
+            self.number_of_non_ok_requests += 1
+            self.log(f'Could not get apps page ({apps_page.status_code}) to create app.', ERROR)
+            return
+        self.number_of_ok_requests += 1
+        soup = BeautifulSoup(apps_page.text, 'html.parser')
+        csrf_token_meta = soup.find('meta', {'name': 'csrf-token'})
+        if isinstance(csrf_token_meta, Tag):
+            authenticty_token = csrf_token_meta.get('content', '')
+            resp = self.session.post(f'https://profile.intra.42.fr/oauth/applications',
+                files={
+                     'authenticity_token': (None, authenticty_token),
+                     'doorkeeper_application[name]': (None, f'ApiHydra_{len(self.apps)}'),
+                     'doorkeeper_application[scopes]': (None, ''),
+                     'doorkeeper_application[redirect_uri]': (None, 'http://127.0.0.1:80'),
+                })
+            if resp.status_code == 201:
+                app_id = str(resp.json()['id'])
+                app_name = resp.json()['name']
+                self.apps[app_id]['id'] = app_id
+                self.update_app(app_id)
+                self.log(f'Created app "{app_name}" ({app_id}).', INFO)
+            else:
+                self.log(f'Could not create app ({resp.status_code}).', ERROR)
+        else:
+            self.log(f'Could not create app, could not find authenticity token.', ERROR)
+
+    def update(self) -> None:
         """Fully update the credentials and tokens of all apps that are available
            through the intra.
         """
-        self.session = self.create_intra_session(self.intra_login, self.intra_password)
+        self.ensure_session()
         app_ids = self.get_app_ids()
         for app_id in app_ids:
             self.log(f'Adding app "{app_id}" to apps list.', INFO)
-            uid, secret = self.get_credentials(app_id)
-            self.log(f'- {uid=}', DEBUG)
-            self.log(f'- {secret=}\n', DEBUG)
-            self.apps[app_id]['uid'] = uid
-            self.apps[app_id]['secret'] = secret
             self.apps[app_id]['id'] = app_id
+            self.update_app(app_id)
         self.refresh_tokens()
+        self.is_updated = True
