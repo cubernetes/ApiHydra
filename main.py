e60b652dece063f02db67d101aeee5c0006f42dd
tosu <timo42@proton.me>
2023-11-15 12:35:39 +0100

    Typo in main.py

--- main.py
+++ main.py
@@ -21,18 +21,18 @@ hydra = FtApiHydra(
     stats=True,
     max_retries=50,
     requests_per_second=1.9,
-    min_request_delay=.02,
+    min_request_delay=.01,
     log_level=logging.INFO,
     intra_login=INTRA_LOGIN,
     intra_password=INTRA_PW,
     responses_file_path_template='./output_%s_%s.json',
 )
 
-hydra.set_number_of_apps(50)
+hydra.set_number_of_apps(100)
 
 campus = 'berlin'
 max_pages = 50
-for page in range(1,21): # we have less than 2000 == 20*100 users in berlin
+for page in range(1,21): # we have less than 5000 == 50*100 users in berlin
     print(end=f'\rGetting users from campus "{campus}", page: {page}/{max_pages}          ')
     hydra.get(f'/campus/{campus}/users?page={page}&per_page=100')
 resps = hydra.get_responses()
